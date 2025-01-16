e86aecf0771c2d13e8db636f138646fb200442eb
EisukeAkimoto <ml@andask.net>
2025-01-16 20:07:34 +0900

    Revert "Merge branch 'production' into dev"
    This reverts commit 4491710d0baa66c6a29b79fc90f2c960c5cb7252, reversing
    changes made to 07be24369e965f33f8122dd7a22b2464e46fef47.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+// import Image from "next/image";
 // import styles from "./page.module.scss";
 
 // import RogixIllust from "../components/img/rogix_illust.png";
