09f580fc7a0c22406a17d9230d7533b5a82427e0
EisukeAkimoto <ml@andask.net>
2025-01-18 16:06:31 +0900

    modify card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -28,19 +28,19 @@ export const Card = async ({ article }) => {
       <Link href={`/articles/${article.id}`} className="flex justify-around space-x-6" scroll={true} >
         <span className="w-28 h-0.5 bg-gray1 dark:bg-gray2 mt-5"></span>
         <div className="w-2/3">
-          <div className="flex flex-col lg:flex-row gap-5 justify-between">
-            <div className="flex-grow">
-              <h2 className="font-normal text-gray-700 dark:text-gray1 text-3xl md:text-4xl mb-5">{article.title}</h2>
-              <p className="font-serif text-gray-700 dark:text-gray1">{article.category.name}</p>
+          <div className="relative overflow-hidden card ">
+            <div className="relative py-2 z-20">
+              <h2 className="font-normal text-gray-700 dark:text-gray0 text-3xl md:text-4xl mb-5 drop-shadow-sm">{article.title}</h2>
+              <p className="font-serif text-gray-700 dark:text-gray1 drop-shadow-sm">{article.category.name}</p>
             </div>
-            <div className="flex-shrink">
+            <div className="absolute -inset-y-1/4 right-0 transparent-thumb dark:transparent-thumb-dark transition-all">
               <Image
-                className="object-cover w-48 h-48 shadow"
+                className="object-cover z-10 transition-all"
                 src={article.thumbnail.url}
                 alt={article.title}
                 height={article.thumbnail.height}
                 width={article.thumbnail.width}
-                style={{ maxWidth: '300px', maxHeight: '200px' }}
+                style={{ maxWidth: '100%', maxHeight: '100%' }}
                 priority
               />
             </div>
