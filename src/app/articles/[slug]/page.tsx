b77ac99283a9a5d89fd99a118848eedc53dc813a
EisukeAkimoto <ml@andask.net>
2025-01-18 16:46:49 +0900

    add back to top

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from 'next/link';
 import parse from "html-react-parser";
+import { IoMdArrowDropleft } from "react-icons/io";
 // import styles from "./page.module.scss";
 import { Metadata } from "next";
 import { getArticlesList, getArticlesDetail } from "@/libs/microcms";
@@ -39,6 +41,7 @@ export default async function Page({ params }) {
   // URLパラメータのIDを参照して、ブログの詳細を取得
   const article = await getArticlesDetail(params.slug);
   return (
+    <>
     <main className="relative">
       <div className=" dark:transparent-img-dark absolute w-full">
           <Image
@@ -60,6 +63,19 @@ export default async function Page({ params }) {
         </div>
       </article>
     </main>
+    <div className=" mt-40">
+          <Link href="/" title="Top page"
+              className="back-btn font-normal relative text-center block bg-gray2 dark:bg-gray4 border border-gray4 dark:border-gray2 cursor-pointer overflow-hidden hover:bg-gray1 hover:border-gray0  hover:shadow-gray0 active:bg-gray0 active:text-gray1"
+          >
+              <span className="relative block  px-5 py-3 whitespace-nowrap no-underline z-20 dark:text-gray2 hover:text-gray0">
+              <IoMdArrowDropleft className="inline-block align-top" />
+              Back to top
+              
+              </span>
+              <div className="mask dark:bg-gray0 h-7 w-40 absolute z-10"></div>
+          </Link>
+      </div>
+    </>
   );
 }
 
